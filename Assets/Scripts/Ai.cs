767eea0055a84451215dd686688e0b3488b89d91
Jalenjones015 <jalenjones015@gmail.com>
2021-03-07 17:33:09 -0500

    Initial commit

---
+++ Assets/Scripts/Ai.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Ai : MonoBehaviour
+{
+    public Transform Play;
+    public GameObject Enemy;
+    NavMeshAgent en;
+
+    void Start()
+    {
+        en = GetComponent<NavMeshAgent>();
+        
+    }
+
+    private void Update()
+    {
+        en.SetDestination(Play.position);
+    }
+
+    public void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Hands")
+        {
+            Destroy(Enemy);
+        }
+    }
+
+}
+
+
