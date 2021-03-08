7f0bb38f46f6cee69c997606e8382248f10609ce
Jalenjones015 <jalenjones015@gmail.com>
2021-03-08 00:25:26 -0500

    Menu update

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    public string NextScene;
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        Debug.Log("Start");
+    }
+
+    public void close()
+    {
+        Application.Quit();
+        Debug.Log("Quit");
+    }
+
+    public void back()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+        Debug.Log("Back");
+    }
+}
