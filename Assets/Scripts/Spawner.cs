2482db014bffd15d3b3577293fd3d1e22f01f166
Jalenjones015 <jalenjones015@gmail.com>
2021-03-07 22:54:02 -0500

    Updates

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -6,4 +6,21 @@ public class Spawner : MonoBehaviour
 {
     public Transform[] spawnpoints;
     public GameObject[] enemies;
+    int randomspawnpoint, randommonster;
+    public static bool spawnallowed;
+
+    public void Start()
+    {
+        spawnallowed = true;
+        InvokeRepeating("Spawn", 0f, 1f);
+    }
+
+    void Spawn()
+    {
+        if (spawnallowed)
+        {
+            randommonster = Random.Range(0, enemies.Length);
+            Instantiate(enemies[randommonster], spawnpoints[randomspawnpoint].position, Quaternion.identity);
+        }
+    }
 }
