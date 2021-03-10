b7fb8fbc0081698aaf5f4724a37272551ab5c0a3
Jalenjones015 <jalenjones015@gmail.com>
2021-03-10 13:49:23 -0500

    Update

--- Assets/Scripts/Attack.cs
+++ Assets/Scripts/Attack.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class Attack : MonoBehaviour
 {
-    float rotate = 50f;
     public Transform point;
     public GameObject bulletpre;
     public float bullforce = 0f;
--- Assets/Scripts/HandsHealth.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class HandsHealth : MonoBehaviour
-{
-    public float maxhealth = 100f;
-    public float curhp = 0f;
-}
