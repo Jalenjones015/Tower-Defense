27b856f19d0d0c32d6612b602d4d751dddcd0ac5
Jalenjones015 <jalenjones015@gmail.com>
2021-03-12 07:31:34 -0500

    Update

---
+++ Assets/Scripts/Money.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Money : MonoBehaviour
+{
+    Store St;
+
+    public void Start()
+    {
+        St = GetComponent<Store>();
+    }
+
+    //public void OnTriggerEnter(Collider other)
+    //{
+    //    if (other.gameObject.CompareTag("Mud"))
+    //    {
+    //        St.mod(5);
+    //        Debug.Log("Money");
+    //    }
+  
+    //}
+
+}
---
+++ Assets/Scripts/Store.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Store : MonoBehaviour
+{
+    public int money;
+    public Text Money1;
+    Store St;
+
+    void Start()
+    {
+        Money1 = GameObject.Find("Money Text").GetComponent<Text>();
+    }
+
+    void Update()
+    {
+        Money1.text = "5" + money.ToString();
+    }
+
+    public int Money()
+    {
+        return money;
+    }
+
+    public void mod(int _money)
+    {
+        money += _money;
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Mud"))
+        {
+            St.mod(5);
+            Debug.Log("Money");
+        }
+
+    }
+}
